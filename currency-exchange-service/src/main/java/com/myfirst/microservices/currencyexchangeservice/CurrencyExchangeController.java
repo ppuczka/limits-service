42bcd8e7f1ef3c2344132d83bc33e152a58a753e
FT27XI <przemyslaw.puczka@ing.com>
2019-02-25 21:50:09 +0100

    added simple curency-conversion controller b

--- currency-exchange-service/src/main/java/com/myfirst/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/myfirst/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,13 +1,11 @@
 package com.myfirst.microservices.currencyexchangeservice;
 
-import com.datastax.driver.core.ResultSet;
 import com.myfirst.microservices.currencyexchangeservice.entity.ExchangeValue;
 import com.myfirst.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +17,7 @@ public class   CurrencyExchangeController {
 	@Autowired
 	private ExchangeValueRepository repository;
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantiy")
 	public ExchangeValue retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
 	ExchangeValue exchangeValue = repository.findBy(2);
 	exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
