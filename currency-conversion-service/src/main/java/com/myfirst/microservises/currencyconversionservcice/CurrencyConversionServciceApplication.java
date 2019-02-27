2dad4cbdc62eb2190bdb6e18d21862e45fb7b151
FT27XI <przemyslaw.puczka@ing.com>
2019-02-27 22:39:24 +0100

    work with exchange service

---
+++ currency-conversion-service/src/main/java/com/myfirst/microservises/currencyconversionservcice/CurrencyConversionServciceApplication.java
@@ -0,0 +1,13 @@
+package com.myfirst.microservises.currencyconversionservcice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServciceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServciceApplication.class, args);
+	}
+
+}
---
+++ currency-conversion-service/src/test/java/com/myfirst/microservises/currencyconversionservcice/CurrencyConversionServciceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myfirst.microservises.currencyconversionservcice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyConversionServciceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
